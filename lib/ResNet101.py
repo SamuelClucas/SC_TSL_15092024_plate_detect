41f01e9a0aef116aae6047ad7c27edc6bab6b27d
SamuelClucas <samuelclucas2001@gmail.com>
2024-10-05 15:30:04 +0100

    updating working tree with renamed files

--- lib/ResNet101.py
+++ lib/ResNet101.py
@@ -1,72 +1,60 @@
----
-title: "ResNet50_Backbone.py Class"
-date: 04-10-2024
-date-format: short
----
-
-### Purpose: 
-The following defines a class used to instantiate a neural network based on google's [cleargrasp](https://sites.google.com/view/cleargrasp). Its pre-trained backbone may be particularly suited to the recognition of transparent microbe plates. I believe a faster R-CNN with a cleargrasp pre-trained backbone will outperform one with a [COCO](https://cocodataset.org/#home) pre-trained resnet50 backbone given the former's feature maps should in principle identify regions of an input image with the traits of a transparent object (for example, glare, or refraction of light through the object). [Here](https://github.com/Shreeyak/cleargrasp) is the code. \
-- Google wrote the model using pytorch, however it is not 'out-of-the-box' configured for bounding box-style object detection; but rather comprises three modules used for object masking, occusion boundary detection, and identification of surface normals. For this reason, I want to extract the backbone only, freeze its pre-trained weights, and then configure successive RPN modules and classification heads.
-
-```{python}
 import math
 import torch.nn as nn
+import torch
 import torch.utils.model_zoo as model_zoo
-from lib.sync_batch_norm import SynchronizedBatchNorm2d
-```
+from .sync_batch_norm import SynchronizedBatchNorm2d
 
-**Breakdown:** \
-- The class defined in succeeding code blocks is derived from the available code [here](https://github.com/Shreeyak/cleargrasp/blob/master/pytorch_networks/masks/modeling/backbone/resnet.py), and is in itself a [resnet-based architecture](https://www.run.ai/guides/deep-learning-for-computer-vision/pytorch-resnet). Residual blocks avoid the problem of vanishing/exploding gradients problematic during backpropagation seen with VGG-based architectures as the number of layers present in the model increase. \
-- The [default pytorch training 'recipe' for ResNet50](https://pytorch.org/blog/how-to-train-state-of-the-art-models-using-torchvision-latest-primitives/) resulted in a top-1 accuracy of 80.9%. \
-
-#### Bottleneck Layer:
-```{python}
 class Bottleneck(nn.Module):
     expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, dilation=1, downsample=None, BatchNorm=None):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = BatchNorm(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               dilation=dilation, padding=dilation, bias=False)
-        self.bn2 = BatchNorm(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = BatchNorm(planes * 4)
-        self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
-        self.stride = stride
-        self.dilation = dilation
+        # First convolution layer with 1x1 kernel, compresses input features (inplanes) to planes
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        # Batch normalization after the first convolution
+        self.bn1 = BatchNorm(planes)
+        # Second convolution layer with 3x3 kernel, processes compressed features
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                       dilation=dilation, padding=dilation, bias=False)
+        # Batch normalization after the second convolution
+        self.bn2 = BatchNorm(planes)
+        # Third convolution layer with 1x1 kernel, expands features back to planes * 4
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        # Batch normalization after the third convolution
+        self.bn3 = BatchNorm(planes * 4)
+        # ReLU activation applied in-place
+        self.relu = nn.ReLU(inplace=True)
+
 
     def forward(self, x):
-        residual = x
-
+        residual = x  # Store the input to be added back later
+    
+    # First conv1 -> BN1 -> ReLU
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-
+    
+    # Second conv2 -> BN2 -> ReLU
         out = self.conv2(out)
         out = self.bn2(out)
         out = self.relu(out)
-
+    
+    # Third conv3 -> BN3 (no ReLU after this step)
         out = self.conv3(out)
         out = self.bn3(out)
-
+    
+    # Apply downsample if necessary (for matching dimensions)
         if self.downsample is not None:
             residual = self.downsample(x)
-
+    
+    # Add the residual to the output
         out += residual
+    # Final ReLU activation
         out = self.relu(out)
 
         return out
-```
-**Breakdown:** \
-- Firstly, a bottleneck class is defined. Bottleneck layers may play numerous distinct roles in a neural networks, but fundamentally they serve to constrain dataflow between network components by downsizing with respect to the number of nodes in the bottleneck layer relative to the preceding layer. Why is this useful? For example, think of two images which are identical in every way but their resolution - one is 1080p, the other 4K. Both images are essentially similarly visually comprehensible, yet there are far more pixels in the 4K image. The only difference is microscopic detail. From this, it could be suggested that the pixels that comprise a 4k image contain less 'information' as far as understanding the contents of the image, relative to the lower resolution duplicate. They both successfully communicate the semantics of the image, where one does so more 'concisely'. Further to this, passing a lower-resolution image with far fewer pixel (eg:250x250) through a network is far more computationally efficient in terms of time complexity by reducing network parameters as much as possible without compromising feature extraction. \
 
-#### ResNet Backbone:
-*Note:* this is almost identical to the class provided [here](https://github.com/Shreeyak/cleargrasp/blob/master/pytorch_networks/masks/modeling/backbone/resnet.py), the only modification being that the pretrained model is not downloaded from the [pytorch models site](https://download.pytorch.org/models/resnet101-5d3b4d8f.pth) directly within the code due to HPC constraints. It is instead loaded from a local .pth file in a models directory. It is worth noting that I'm taking code written for their mask module, but I believe analogous classes defined in the other module directories (occlusion boundaries, surface normals) are identical. I need to train the model on the [transparent object dataset](https://sites.google.com/view/transparent-objects) for the convolutional layer (for feature map extraction). 
-- The .pth file download link: https://download.pytorch.org/models/resnet50-11ad3fa6.pth \
-```{python}
 class ResNet(nn.Module):
 
     def __init__(self, block, layers, output_stride, BatchNorm, model_dir, pretrained=True):
@@ -169,10 +157,8 @@ class ResNet(nn.Module):
                 model_dict[k] = v
         state_dict.update(model_dict)
         self.load_state_dict(state_dict)
-```
 
-#### Construct ResNet-101 Model:
-```{python}
+# Get instance of Resnet50
 def ResNet101(model_dir, output_stride, BatchNorm, pretrained=True):
     """Constructs a ResNet-101 model.
     Args:
@@ -180,25 +166,3 @@ def ResNet101(model_dir, output_stride, BatchNorm, pretrained=True):
     """
     model = ResNet(Bottleneck, [3, 4, 23, 3], output_stride, BatchNorm, model_dir, pretrained=pretrained)
     return model
-```
-**Breakdown:** \
-- The model's weights are initially set to those trained on the [ImageNet1K](https://www.image-net.org/) dataset, version 2. \
-- Provided within the repo is the artificial image set on which cleargrasp was trained to reliably identify transparent objects. After this training, the backbone weights are frozen.
-
-#### Main:
-Below outlines how an object of this class is instantiated. Make sure to download the model's .pth file first:
-```{bash}
-curl -O https://download.pytorch.org/models/resnet50-11ad3fa6.pth 
-```
-Then change 'model_dir' string to path/to/your/model/.pth/file
-```{python}
-if __name__ == "__main__":
-    import torch
-    model_dir = 'resnet50-11ad3fa6.pth'
-    model = ResNet101(model_dir, BatchNorm=nn.BatchNorm2d, pretrained=True, output_stride=8)
-    input = torch.rand(1, 3, 512, 512)
-    output, low_level_feat = model(input)
-    print(output.size())
-    print(low_level_feat.size())
-```
-**Next step:** write a script to instantiate a model and load dataset before training.
