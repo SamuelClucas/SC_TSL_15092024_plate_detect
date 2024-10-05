6e40afd78c7ef5ef43bd3e0c58ab2c1abedd6074
SamuelClucas <samuelclucas2001@gmail.com>
2024-10-05 14:39:38 +0100

    model loading with optimizer, preprocess transforms for trainign and validation, and criterion

--- scripts/model_implementation/training_setup_ResNet50.py
+++ scripts/model_implementation/training_setup_ResNet50.py
@@ -1,22 +1,27 @@
----
-title: "Setup Pre-Training ResNet50 Model"
-date: 05-10-2024
-date-format: short
----
-
-### Purpose:
-Before training, the model must be loaded. 
-
-#### Imports:
-```{python}
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR
+import torchvision.transforms as transforms
 from lib.ResNet50 import ResNet, get_ResNet
-```
 
-#### Main
-```{python}
+from src import Plate_Image_Dataset, helper_training_functions
+
+from torchvision.transforms.v2 import functional as F
+from pathlib import Path
+from typing import Dict, Union
+from torchvision.transforms import v2 as T
+import lib.utils as utils
+from lib.engine import train_one_epoch, evaluate 
+import os
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+import PIL
+from torchvision.utils import draw_bounding_boxes
+from torchvision.io import read_image
+
+
 if __name__ == "__main__":
     model_dir = 'resnet50-11ad3fa6.pth'
     model = get_ResNet(model_dir, BatchNorm=nn.BatchNorm2d, pretrained=True, output_stride=8)
@@ -28,6 +33,8 @@ if __name__ == "__main__":
 
     model.eval()  # or 
     model.train() # if training
+
+# see recipe here: https://pytorch.org/blog/how-to-train-state-of-the-art-models-using-torchvision-latest-primitives/
 
 #  Setup optimizer and learning rate scheduler
     optimizer = optim.SGD(model.parameters(), lr=0.5, momentum=0.9, weight_decay=2e-05)
@@ -49,4 +56,3 @@ if __name__ == "__main__":
         transforms.CenterCrop(224),
         transforms.ToTensor(),
     ])
-```
